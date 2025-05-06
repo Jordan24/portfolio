9ad183708a4133c07c1d56cb3175f7987b88914e
Jordan Szymczyk <jordan24@gmail.com>
2025-05-06 12:49:36 -0600

    Analyze fixes

--- dataconnect-generated/dart/default_connector/default.dart
+++ dataconnect-generated/dart/default_connector/default.dart
@@ -1,15 +1,8 @@
-library default_connector;
 import 'package:firebase_data_connect/firebase_data_connect.dart';
-import 'dart:convert';
-
-
-
-
-
 
 
 class DefaultConnector {
-  
+
 
   static ConnectorConfig connectorConfig = ConnectorConfig(
     'us-central1',
