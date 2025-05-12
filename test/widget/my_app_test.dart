552754f70ba294c9f73e3d48ccdbd0a6346a2589
Jordan Szymczyk <jordan24@gmail.com>
2025-05-12 12:19:58 -0600

    Update tests

---
+++ test/widget/my_app_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:portfolio/main.dart';
+
+void main() {
+  testWidgets('Finds a Button with text "Push Me"', (WidgetTester tester) async {
+    // Build the app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that a Button with the text "Push Me" is present.
+    expect(find.widgetWithText(ElevatedButton, 'Push Me'), findsOneWidget);
+  });
+}
