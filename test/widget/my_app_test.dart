0bc43b98885f63a976a1134b3516c621f4709e31
Jordan Szymczyk <jordan24@gmail.com>
2025-05-12 12:24:04 -0600

    Update test

--- test/widget/my_app_test.dart
+++ test/widget/my_app_test.dart
@@ -3,11 +3,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:portfolio/main.dart';
 
 void main() {
-  testWidgets('Finds a Button with text "Push Me"', (WidgetTester tester) async {
+  testWidgets('Finds a Button with text "Coming Soon"', (
+    WidgetTester tester,
+  ) async {
     // Build the app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
     // Verify that a Button with the text "Push Me" is present.
-    expect(find.widgetWithText(ElevatedButton, 'Push Me'), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, 'Coming Soon'), findsOneWidget);
   });
 }
