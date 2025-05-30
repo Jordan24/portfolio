e71b39c038487c42bd3ef1e33f10c0821fbe57f5
Jordan Szymczyk <jordan24@gmail.com>
2025-05-30 16:20:07 -0600

    refactor: Remove unused import from my_app_test.dart

--- test/widget/my_app_test.dart
+++ test/widget/my_app_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:portfolio/main.dart';
 import 'package:portfolio/pages/portfolio_screen.dart';
 
 void main() {
