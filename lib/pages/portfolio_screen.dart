4830089da0d21f4d796a41862f943fdf921e2f81
Jordan Szymczyk <jordan24@gmail.com>
2025-05-30 16:17:08 -0600

    refactor: Update PortfolioScreen widget and tests for improved structure and readability

--- lib/pages/portfolio_screen.dart
+++ lib/pages/portfolio_screen.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class PortfolioScreen extends StatefulWidget {
-  const PortfolioScreen({super.key, required this.title, required this.onThemeColorChanged, required this.currentThemeColor});
+  const PortfolioScreen({
+    super.key,
+    required this.title,
+    required this.onThemeColorChanged,
+    required this.currentThemeColor,
+  });
   final String title;
   final void Function(Color) onThemeColorChanged;
   final Color currentThemeColor;
@@ -48,77 +53,71 @@ class _PortfolioScreenState extends State<PortfolioScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(
-          widget.title,
-          style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.color_lens,
-              color: Theme.of(context).colorScheme.onPrimary,
-            ),
-            iconSize: 30.0,
-            onPressed: _showColorPickerDialog,
-            tooltip: 'Change Theme Color',
-          ),
-        ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: widget.currentThemeColor),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Theme.of(context).colorScheme.surface,
-              Theme.of(context).colorScheme.surface,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: Text(
+            widget.title,
+            style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
           ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.color_lens,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              iconSize: 30.0,
+              onPressed: _showColorPickerDialog,
+              tooltip: 'Change Theme Color',
+            ),
+          ],
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: _incrementCounter,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Theme.of(context).colorScheme.primary,
-                  foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                ),
-                child: const Text(
-                  'Coming Soon',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Theme.of(context).colorScheme.surface,
+                Theme.of(context).colorScheme.surface,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _incrementCounter,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                  child: const Text(
+                    'Coming Soon',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'You have pushed the button this many times:',
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onSurface,
-                  fontSize: 20,
+                const SizedBox(height: 20),
+                Text(
+                  'You have pushed the button this many times:',
+                  style: TextStyle(fontSize: 20),
                 ),
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                  color: Theme.of(context).colorScheme.onSurface,
-                  fontSize: 40,
-                ),
-              ),
-            ],
+                Text('$_counter', style: TextStyle(fontSize: 40)),
+              ],
+            ),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
--- test/widget/my_app_test.dart
+++ test/widget/my_app_test.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:portfolio/main.dart';
+import 'package:portfolio/pages/portfolio_screen.dart';
 
 void main() {
   testWidgets('Finds a Button with text "Coming Soon"', (
     WidgetTester tester,
   ) async {
     // Build the app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      PortfolioScreen(
+        title: '',
+        onThemeColorChanged: (Color color) {},
+        currentThemeColor: Colors.black,
+      ),
+    );
 
     // Verify that a Button with the text "Push Me" is present.
     expect(find.widgetWithText(ElevatedButton, 'Coming Soon'), findsOneWidget);
@@ -15,7 +22,13 @@ void main() {
 
   testWidgets('AppBar IconButton has correct iconSize and color', (WidgetTester tester) async {
     // Build the app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      PortfolioScreen(
+        title: '',
+        onThemeColorChanged: (Color color) {},
+        currentThemeColor: Colors.black,
+      ),
+    );
 
     // Find the IconButton by its icon.
     final iconButtonFinder = find.widgetWithIcon(IconButton, Icons.color_lens);
