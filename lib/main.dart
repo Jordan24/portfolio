f291ff4286077b203d07b9aa749d5b0fc0a4f664
Jordan Szymczyk <jordan24@gmail.com>
2025-06-05 17:04:33 -0600

    Change names on MyApp and AppTheme

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,18 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/pages/welcome_screen.dart';
 import 'package:portfolio/providers/theme_mode_provider.dart';
 import 'package:portfolio/providers/theme_color_provider.dart';
-import 'package:portfolio/theme/color_scheme.dart';
+import 'package:portfolio/theme/app_theme.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: PortfolioApp()));
 }
 
 num add(num a, num b) {
   return a + b;
 }
 
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+class PortfolioApp extends ConsumerWidget {
+  const PortfolioApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
