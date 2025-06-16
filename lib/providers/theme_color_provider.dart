26debd4ba72276e4f501078545d396d1680f040c
Jordan Szymczyk <jordan24@gmail.com>
2025-06-16 11:01:30 -0600

    Refactor theme color and mode providers to load and save preferences using SharedPreferences

--- lib/providers/theme_color_provider.dart
+++ lib/providers/theme_color_provider.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeColorNotifier extends StateNotifier<Color> {
-  ThemeColorNotifier() : super(const Color.fromARGB(255, 0, 32, 58));
+
+  Future<void> _loadThemeColor() async {
+    final prefs = await SharedPreferences.getInstance();
+    final int? colorValue = prefs.getInt('theme_color');
+    if (colorValue != null) {
+      state = Color(colorValue);
+    }
+  }
+
+  Future<void> _saveThemeColor(Color color) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('theme_color', color.value);
+  }
+
+  ThemeColorNotifier() : super(const Color.fromARGB(255, 0, 32, 58)) {
+    _loadThemeColor();
+  }
 
   void setColor(Color color) {
     state = color;
+    _saveThemeColor(color);
   }
 }
 
