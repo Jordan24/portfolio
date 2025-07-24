30240c781826cb4cfad946365a747ff1290902e5
Jordan Szymczyk <jordan24@gmail.com>
2025-07-24 17:13:56 -0600

    Update auth provider to store user data

--- lib/user/providers/auth_provider.dart
+++ lib/user/providers/auth_provider.dart
@@ -1,46 +1,104 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:portfolio/user/models/user.dart' as model;
 
-class AuthNotifier extends StateNotifier<bool> {
-  AuthNotifier() : super(FirebaseAuth.instance.currentUser != null) {
-    FirebaseAuth.instance.authStateChanges().listen((user) {
-      state = user != null;
-    });
+class AuthNotifier extends StateNotifier<model.User?> {
+  AuthNotifier() : super(null) {
+    _loadUser();
+  }
+
+  Future<void> _loadUser() async {
+    final firebaseUser = FirebaseAuth.instance.currentUser;
+    if (firebaseUser != null) {
+      final userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(firebaseUser.uid)
+          .get();
+      if (userDoc.exists) {
+        state = model.User.fromJson(firebaseUser.uid, userDoc.data()!);
+      } else {
+        // This case might happen if the user was created in auth but the document wasn't created in firestore
+        final newUser = model.User(
+          id: firebaseUser.uid,
+          email: firebaseUser.email!,
+        );
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(newUser.id)
+            .set(newUser.toJson());
+        state = newUser;
+      }
+    }
   }
 
   Future<void> signIn(String email, String password) async {
     try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
+      final userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
-    } on FirebaseAuthException catch (error) {
-      throw FirebaseAuthException(
-        code: error.code,
-        message: error.message ?? 'Authentication failed.',
-      );
+      final userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userCredential.user!.uid)
+          .get();
+      if (userDoc.exists) {
+        state = model.User.fromJson(userCredential.user!.uid, userDoc.data()!);
+      } else {
+        final newUser = model.User(
+          id: userCredential.user!.uid,
+          email: email,
+        );
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(newUser.id)
+            .set(newUser.toJson());
+        state = newUser;
+      }
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.message);
     }
   }
 
-  Future<void> signUp(String email, String password) async {
+  Future<void> signUp(String email, String password, String? username) async {
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
-    } on FirebaseAuthException catch (error) {
-      throw FirebaseAuthException(
-        code: error.code,
-        message: error.message ?? 'Authentication failed.',
+      final newUser = model.User(
+        id: userCredential.user!.uid,
+        username: username,
+        email: email,
       );
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(newUser.id)
+          .set(newUser.toJson());
+      state = newUser;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.message);
     }
   }
 
   Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
+    state = null;
+  }
+
+  Future<void> updateUserData(model.User user) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.id)
+          .update(user.toJson());
+      state = user;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 }
 
-final authProvider = StateNotifierProvider<AuthNotifier, bool>((ref) {
+final authProvider = StateNotifierProvider<AuthNotifier, model.User?>((ref) {
   return AuthNotifier();
 });
