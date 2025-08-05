c47adc8eead54846c924252f5ecfc56c113ae827
Jordan Szymczyk <jordan24@gmail.com>
2025-08-05 11:30:59 -0600

    Add google maps api key to .env

--- lib/env.dart
+++ lib/env.dart
@@ -4,10 +4,12 @@ part 'env.g.dart';
 
 @Envied(path: '.env')
 abstract class Env {
-  @EnviedField(varName: 'WEB_API_KEY', obfuscate: true)
-  static final String webApiKey = _Env.webApiKey;
   @EnviedField(varName: 'ANDROID_API_KEY', obfuscate: true)
   static final String androidApiKey = _Env.androidApiKey;
+  @EnviedField(varName: 'GOOGLE_MAPS_API_KEY', obfuscate: true)
+  static final String googleMapsApiKey = _Env.googleMapsApiKey;
   @EnviedField(varName: 'IOS_API_KEY', obfuscate: true)
   static final String iosApiKey = _Env.iosApiKey;
+  @EnviedField(varName: 'WEB_API_KEY', obfuscate: true)
+  static final String webApiKey = _Env.webApiKey;
 }
