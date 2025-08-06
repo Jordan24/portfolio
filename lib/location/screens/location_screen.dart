61fd3a6ce24378c98c3c9453df3559919b17e5d5
Jordan Szymczyk <jordan24@gmail.com>
2025-08-06 11:20:32 -0600

    Select on map

--- lib/location/screens/location_screen.dart
+++ lib/location/screens/location_screen.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:portfolio/env.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:location/location.dart';
 import 'package:portfolio/location/models/place_location_model.dart';
+import 'package:portfolio/location/screens/map_screen.dart';
 
 class LocationScreen extends StatefulWidget {
   const LocationScreen({super.key});
@@ -21,47 +23,10 @@ class _LocationScreenState extends State<LocationScreen> {
     if (_pickedLocation == null) return '';
     final lat = _pickedLocation!.latitude;
     final lon = _pickedLocation!.longitude;
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lon&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:A%7C$lat,$lon&key=${Env.googleMapsApiKey}';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lon&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7C%7C$lat,$lon&key=${Env.googleMapsApiKey}';
   }
 
-  void getCurrentLocation() async {
-    Location location = Location();
-
-    bool serviceEnabled;
-    PermissionStatus permissionGranted;
-    LocationData locationData;
-
-    serviceEnabled = await location.serviceEnabled();
-    if (!serviceEnabled) {
-      serviceEnabled = await location.requestService();
-      if (!serviceEnabled) {
-        return;
-      }
-    }
-
-    permissionGranted = await location.hasPermission();
-    if (permissionGranted == PermissionStatus.denied) {
-      permissionGranted = await location.requestPermission();
-      if (permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
-
-    setState(() {
-      _isLoading = true;
-    });
-
-    locationData = await location.getLocation();
-    final lat = locationData.latitude;
-    final lon = locationData.longitude;
-
-    if (lat == null || lon == null) {
-      setState(() {
-        _isLoading = false;
-      });
-      return;
-    }
-
+  Future<void> _savePlace(double lat, double lon) async {
     final url = Uri.parse(
       'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lon&key=${Env.googleMapsApiKey}',
     );
@@ -77,6 +42,49 @@ class _LocationScreenState extends State<LocationScreen> {
       );
       _isLoading = false;
     });
+  }
+
+  Future<PlaceLocation?> getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return null;
+      }
+    }
+
+    permissionGranted = await location.requestPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lon = locationData.longitude;
+
+    if (lat == null || lon == null) {
+      setState(() {
+        _isLoading = false;
+      });
+      return null;
+    }
+
+    _savePlace(lat, lon);
+
+    return PlaceLocation(latitude: lat, longitude: lon, address: '');
   }
 
   @override
@@ -108,33 +116,62 @@ class _LocationScreenState extends State<LocationScreen> {
       locationContent = const Center(child: CircularProgressIndicator());
     }
 
+    void selectOnMap() async {
+      final PlaceLocation? currentLocation =
+          _pickedLocation ?? await getCurrentLocation();
+      final selectedLocation = await Navigator.of(context).push<LatLng>(
+        MaterialPageRoute(
+          builder:
+              (ctx) => MapScreen(
+                location: PlaceLocation(
+                  latitude: currentLocation?.latitude ?? 37.422,
+                  longitude: currentLocation?.longitude ?? -122.084,
+                  address: "",
+                ),
+              ),
+        ),
+      );
+
+      if (selectedLocation == null) return;
+
+      _savePlace(selectedLocation.latitude, selectedLocation.longitude);
+    }
+
     return Scaffold(
       appBar: AppBar(title: Text('Location')),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
+          spacing: 16,
           children: [
-            Container(
-              height: 240,
-              width: double.infinity,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                border: Border.all(width: 1, color: theme.colorScheme.primary),
+            Center(
+              child: Container(
+                height: 240,
+                width: double.infinity,
+                constraints: const BoxConstraints(maxWidth: 500),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: theme.colorScheme.primary,
+                  ),
+                ),
+                child: locationContent,
               ),
-              child: locationContent,
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextButton.icon(
                   icon: const Icon(Icons.location_on),
                   label: const Text('Get Current Location'),
                   onPressed: getCurrentLocation,
                 ),
+                SizedBox(width: 24),
                 TextButton.icon(
                   icon: const Icon(Icons.map),
                   label: const Text('Select on Map'),
-                  onPressed: () {},
+                  onPressed: selectOnMap,
                 ),
               ],
             ),
